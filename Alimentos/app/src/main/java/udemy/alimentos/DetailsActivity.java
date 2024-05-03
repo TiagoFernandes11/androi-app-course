941db6ba39f7d4138e0b251b43a90fc2ca26c55d
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-05-03 12:55:22 -0300

    Creating food activity

---
+++ Alimentos/app/src/main/java/udemy/alimentos/DetailsActivity.java
@@ -0,0 +1,33 @@
+package udemy.alimentos;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private ViewHolder viewHolder = new ViewHolder();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_details);
+        this.viewHolder.mtextName = findViewById(R.id.text_name);
+        this.viewHolder.mtextCalories = findViewById(R.id.text_calories);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private static class ViewHolder {
+        TextView mtextName;
+        TextView mtextCalories;
+    }
+}
