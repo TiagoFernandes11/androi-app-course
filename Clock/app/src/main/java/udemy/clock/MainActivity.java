b5c16956d70dc8c4fc13f9f99b332acf7fafbc97
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-05-14 11:53:07 -0300

    Update MainActivity.java

--- Clock/app/src/main/java/udemy/clock/MainActivity.java
+++ Clock/app/src/main/java/udemy/clock/MainActivity.java
@@ -50,9 +50,20 @@ public class MainActivity extends AppCompatActivity {
         this.mViewHolder.textBattery = findViewById(R.id.text_batery);
 
         this.registerReceiver(this.mReciever, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_FULLSCREEN);
         this.startClock();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                      View.SYSTEM_UI_FLAG_IMMERSIVE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
     }
 
     private void startClock() {
